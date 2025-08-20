3b04a15bf276aece6ea3960a321255953ac8980b
hageromar <hageromar328@gmail.com>
2025-08-20 19:57:12 +0300

    add book entity to book model

--- lib/Features/home/domain/entity/book_entity.dart
+++ lib/Features/home/domain/entity/book_entity.dart
@@ -2,11 +2,12 @@ class BookEntity {
   final String bookId;
   final String? image;
   final String auther;
+  final String title;
   final num? price;
   final num? count;
 
   BookEntity(
-      {required this.bookId,required this.image,
+      {required this.title,required this.bookId,required this.image,
       required this.auther,
       required this.price,
       required this.count});
