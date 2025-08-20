1a87b206f124f12727f69982c15dd0189251d68e
hageromar <hageromar328@gmail.com>
2025-08-20 21:29:43 +0300

    add hive to bookentity

--- lib/Features/home/domain/entity/book_entity.dart
+++ lib/Features/home/domain/entity/book_entity.dart
@@ -1,10 +1,18 @@
-
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String auther;
+  @HiveField(3)
   final String title;
+  @HiveField(4)
   final num? rate;
+  @HiveField(5)
   final num? count;
 
   BookEntity(
